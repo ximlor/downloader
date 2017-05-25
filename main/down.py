960dac37bff4ab2d80735fe8c90125a213887896
ximlor <2705833399@qq.com>
2017-05-25 12:15:58 +0800

    忽略已存在文件

--- main/down.py
+++ main/down.py
@@ -22,23 +22,27 @@ def log(filename, content):
 
 
 def downLoad(item):
-    url = 'http://120.52.73.73/data1.cache.directory/media/videos/iphone/%s.mp4' % item
-    # url = 'http://120.52.73.73/data1.cache.directory/media/videos/xphone/%s.mp4' % item
+    url = 'http://120.52.73.73/data1.cache.directory/media/filenames/iphone/%s.mp4' % item
+    # url = 'http://120.52.73.73/data1.cache.directory/media/filenames/xphone/%s.mp4' % item
     # url = 'http://img1.gtimg.com/cq/pics/hv1/115/52/2204/143328475.jpg'
-    r = requests.get(url)
+
     log_path = create_path('/storage/log')
-    # log_path = create_path('./log')
     log_file = '%s/mp4.log' % log_path
-    if r.status_code == requests.codes.ok:
-        base_path = create_path('/storage/mp4')
-        video = '%s/%s.mp4' % (base_path, item)
-        # video = '%s/%s.jpg' % (base_path, item)
-        try:
-            write(video, r.content)
-            log(log_file, '%s-0\n' % item)
-        except Exception:
-            log(log_file, 'failed!')
-            print(Exception.message)
-            pass
+
+    base_path = create_path('/storage/mp4')
+    filename = '%s/%s.mp4' % (base_path, item)
+    if os.path.exists(filename):
+        print('exist')
+        pass
     else:
-        log(log_file, '%s-404\n' % item)
+        r = requests.get(url)
+        if r.status_code == requests.codes.ok:
+            try:
+                write(filename, r.content)
+                log(log_file, '%s-0\n' % item)
+            except Exception:
+                log(log_file, 'failed!')
+                print(Exception.message)
+                pass
+        else:
+            log(log_file, '%s-404\n' % item)
